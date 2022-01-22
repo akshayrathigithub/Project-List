c7b206bfb1496079a50f36b53a7f034cf6670d4a
Akshay Rathi <akshayrathi96@gmail.com>
2022-01-22 23:47:34 +0530

    project list initial changes

---
+++ app.js
@@ -0,0 +1,128 @@
+// links
+const cpiSpiCalculatorLinkUrl = "";
+const taxiSharingPlatformLinkUrl = "";
+const socketIoLinkUrl = "";
+const taskManagerLinkUrl = "";
+const portfolioLinkUrl = "https://akshayrathi.com/";
+
+// github links
+const cpiSpiCalculatorGithubLink =
+  "https://github.com/akshayrathigithub/CPI-SPI-Calculator";
+const taxiSharingPlatformGithubLink =
+  "https://github.com/akshayrathigithub/Taxi-Sharing-Platform";
+const socketIoGithubLink =
+  "https://github.com/akshayrathigithub/BoilerPlateSocketIo";
+const taskManagerGithubLink =
+  "https://github.com/akshayrathigithub/Tasks-Manager";
+const reactNativeCalculatorGithubLink =
+  "https://github.com/akshayrathigithub/ReactNative_Calculator";
+const reactDatePickerGithubLink =
+  "https://github.com/akshayrathigithub/React-DatePicker";
+
+// personal contact links
+const githubLink = "https://github.com/akshayrathigithub";
+const linkedinLinkUrl = "https://www.linkedin.com/in/akshay-rathi-sde/";
+
+// btn instances
+/* Portfolio */
+const portFolioLink = document.getElementById("portFolioLink");
+
+/* Task Manager */
+const taskManagerLink = document.getElementById("taskManagerLink");
+const taskManagerGithub = document.getElementById("taskManagerGithub");
+
+/* Socket-Io App */
+const socketIoLink = document.getElementById("socketIoLink");
+const socketIoGithub = document.getElementById("socketIoGithub");
+
+/* React Date Picker */
+const reactDatePickerGithub = document.getElementById("reactDatePickerGithub");
+
+/* React Native Calculator */
+const reactNativeCalculatorGithub = document.getElementById(
+  "reactNativeCalculatorGithub"
+);
+
+/* Taxi Sharing Platform */
+const taxiSharingPlatformLink = document.getElementById(
+  "taxiSharingPlatformLink"
+);
+const taxiSharingPlatformGithub = document.getElementById(
+  "taxiSharingPlatformGithub"
+);
+
+/* CPI-SPI Calculator */
+const cpiSpiCalculatorLink = document.getElementById("cpiSpiCalculatorLink");
+const cpiSpiCalulatorGithub = document.getElementById("cpiSpiCalulatorGithub");
+
+/* Personal Webiste */
+const personalWebsite = document.getElementById("personalWebsite");
+
+/* Github Profile */
+const githubProfileLink = document.getElementById("githubProfileLink");
+
+/* Linkedin Profile */
+const linkedinLink = document.getElementById("linkedinLink");
+
+// add click event listener
+/* Portfolio */
+portFolioLink.addEventListener("click", () => {
+  window.open(portfolioLinkUrl, "_blank");
+});
+
+/* Task Manager */
+taskManagerLink.addEventListener("click", () => {
+  window.open(taskManagerLinkUrl, "_blank");
+});
+taskManagerGithub.addEventListener("click", () => {
+  window.open(taskManagerGithubLink, "_blank");
+});
+
+/* Socket-Io App */
+socketIoLink.addEventListener("click", () => {
+  window.open(socketIoLinkUrl, "_blank");
+});
+socketIoGithub.addEventListener("click", () => {
+  window.open(socketIoGithubLink, "_blank");
+});
+
+/* React Date Picker */
+reactDatePickerGithub.addEventListener("click", () => {
+  window.open(reactDatePickerGithubLink, "_blank");
+});
+
+/* React Native Calculator */
+reactNativeCalculatorGithub.addEventListener("click", () => {
+  window.open(reactNativeCalculatorGithubLink, "_blank");
+});
+
+/* Taxi Sharing Platform */
+taxiSharingPlatformLink.addEventListener("click", () => {
+  window.open(taxiSharingPlatformLink, "_blank");
+});
+taxiSharingPlatformGithub.addEventListener("click", () => {
+  window.open(taxiSharingPlatformGithubLink, "_blank");
+});
+
+/* CPI-SPI Calculator */
+cpiSpiCalculatorLink.addEventListener("click", () => {
+  window.open(cpiSpiCalculatorLinkUrl, "_blank");
+});
+cpiSpiCalulatorGithub.addEventListener("click", () => {
+  window.open(cpiSpiCalculatorGithubLink, "_blank");
+});
+
+/* Personal Webiste */
+personalWebsite.addEventListener("click", () => {
+  window.open(portfolioLinkUrl, "_blank");
+});
+
+/* Github Profile */
+githubProfileLink.addEventListener("click", () => {
+  window.open(githubLink, "_blank");
+});
+
+/* Linkedin Profile */
+linkedinLink.addEventListener("click", () => {
+  window.open(linkedinLinkUrl, "_blank");
+});
